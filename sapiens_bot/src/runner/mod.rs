72ce17a1eab03e9655cefaca48d9520972b59f31
Sebastien Soudan <sebastien.soudan@gmail.com>
2023-05-17 16:17:12 -0700

    feat: support for Palm chat-bison-001 in the bot

--- sapiens_bot/src/runner/mod.rs
+++ sapiens_bot/src/runner/mod.rs
@@ -1,11 +1,14 @@
+use std::env::VarError;
 use std::fmt::Debug;
+use std::str::FromStr;
 use std::sync::Arc;
 
 use sapiens::context::{ChatEntryFormatter, ChatHistoryDump};
+use sapiens::models::SupportedModel;
 use sapiens::runner::Chain;
 use sapiens::tools::TerminationMessage;
 use sapiens::{
-    wrap_observer, Error, InvocationFailureNotification, InvocationSuccessNotification,
+    models, wrap_observer, Error, InvocationFailureNotification, InvocationSuccessNotification,
     ModelUpdateNotification, SapiensConfig, StepObserver, StepOrStop, WeakStepObserver,
 };
 use serenity::futures::channel::mpsc;
@@ -30,7 +33,43 @@ impl SapiensBot {
         let _ =
             std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set in configuration file");
 
-        let config = SapiensConfig::default();
+        let temperature = Some(0.);
+
+        let model = match std::env::var("MODEL") {
+            Ok(e) => SupportedModel::from_str(&e).expect("Invalid model"),
+            Err(e) => {
+                if e == VarError::NotPresent {
+                    warn!("MODEL not specified: defaulting to chat-bison-001.");
+                    SupportedModel::ChatBison001
+                } else {
+                    panic!("Invalid model: {}", e)
+                }
+            }
+        };
+
+        let model = match model {
+            SupportedModel::ChatBison001 => {
+                let google_api_key =
+                    std::env::var("GOOGLE_API_KEY").expect("GOOGLE_API_KEY is not set");
+
+                models::vertex_ai::build(google_api_key, temperature)
+                    .await
+                    .expect("Failed to build model")
+            }
+            _ => {
+                let api_key = std::env::var("OPENAI_API_KEY").ok();
+                let api_base = std::env::var("OPENAI_API_BASE").ok();
+
+                models::openai::build(model, api_key, api_base, temperature)
+                    .await
+                    .expect("Failed to build model")
+            }
+        };
+
+        let config = SapiensConfig {
+            model,
+            ..SapiensConfig::default()
+        };
         let chain = Chain::new(toolbox, config).await;
 
         Self { chain }
