596b551693ef70c78ecb8350a9eb77fc1178e712
Sebastien Soudan <sebastien.soudan@gmail.com>
2024-04-27 19:11:24 -0700

    chore(deps): upgraded serenity

--- sapiens_bot/src/commands/ping.rs
+++ sapiens_bot/src/commands/ping.rs
@@ -1,10 +1,9 @@
-use serenity::builder::CreateApplicationCommand;
-use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use serenity::all::{CommandDataOption, CreateCommand};
 
 pub fn run(_options: &[CommandDataOption]) -> String {
     "Hey, I'm alive!".to_string()
 }
 
-pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command.name("ping").description("A ping command")
+pub fn register() -> CreateCommand {
+    CreateCommand::new("ping").description("A ping command")
 }
